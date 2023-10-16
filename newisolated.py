d2d097b681718d7ef8281f9224cc78196f02408c
hackdkarbon <112771090+hackdkarbon@users.noreply.github.com>
2023-10-16 11:37:40 +0530

    Add files via upload

--- newisolated.py
+++ newisolated.py
@@ -53,49 +53,89 @@ class DataProcessor:
       # If conversion with ipaddress fails, falls back to regex
       return bool(re.match(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$', str(ip_str)))
 
+# =============================================================================
+#   def identify_and_convert_columns(self):
+#     try:
+#        self.categorical_cols = self.df.select_dtypes(
+#               include=['object']).columns.tolist()
+#        self.continuous_cols = self.df.select_dtypes(
+#               include=['float64', 'int64']).columns.tolist()
+#     
+#       for col in self.categorical_cols:
+#         # Improved robust datetime check:
+#         is_datetime = False
+#         try:
+#            for col in df.columns:
+#              if df[col].dtype == 'object':
+#                try:
+#                   df[col] = pd.to_datetime(df[col])
+#                 except ValueError:
+#                   pass
+#          is_datetime = True
+#          except Exception as e:
+#           print("An exception occurred: ", e)
+#           #pd.to_datetime(self.df[col], errors='coerce').notna())
+#           #df['date_column'] = pd.to_datetime(df['date_column']
+#          except Exception as e:
+#           logging.warning(
+#               f"Unable to check datetime for column {col}: {str(e)}")
+# 
+#         if is_datetime:
+#           self.df[col] = pd.to_datetime(self.df[col]).astype(int) / 10**9
+#           if col not in self.continuous_cols:
+#             self.continuous_cols.append(col)
+#           self.categorical_cols.remove(col)
+# 
+#       logging.info("Columns identified and converted where necessary.")
+# 
+#       # Assertions to verify column names
+#       assert all(col in self.df.columns for col in self.categorical_cols
+#                  ), "Some categorical columns are not in DataFrame"
+#       assert all(
+#           col in self.df.columns for col in
+#           self.continuous_cols), "Some continuous columns are not in DataFrame"
+#     except Exception as e:
+#       logging.error(f"Error in identifying and converting columns: {str(e)}")
+#       raise
+# =============================================================================
   def identify_and_convert_columns(self):
     try:
-      self.categorical_cols = self.df.select_dtypes(
-          include=['object']).columns.tolist()
-      self.continuous_cols = self.df.select_dtypes(
-          include=['float64', 'int64']).columns.tolist()
+        self.categorical_cols = self.df.select_dtypes(
+            include=['object']).columns.tolist()
+        self.continuous_cols = self.df.select_dtypes(
+            include=['float64', 'int64']).columns.tolist()
 
-      for col in self.categorical_cols:
-        # Improved robust datetime check:
-        is_datetime = False
-        try:
-           for col in df.columns:
-             if df[col].dtype == 'object':
-               try:
-                 df[col] = pd.to_datetime(df[col])
-               except ValueError:
-                 pass
-        is_datetime = True
-         except Exception as e:
-          print("An exception occurred: ", e)
-          #pd.to_datetime(self.df[col], errors='coerce').notna())
-          #df['date_column'] = pd.to_datetime(df['date_column']
-         except Exception as e:
-          logging.warning(
-              f"Unable to check datetime for column {col}: {str(e)}")
+        for col in self.categorical_cols:
+            # Improved robust datetime check:
+            is_datetime = False
+            try:
+                for column in self.df.columns:  # <-- Changed `df` to `self.df` here
+                    if self.df[column].dtype == 'object':  # <-- Changed `df` to `self.df` here
+                        try:
+                            self.df[column] = pd.to_datetime(self.df[column])  # <-- Changed `df` to `self.df` here
+                            is_datetime = True
+                        except ValueError:
+                            pass
+            except Exception as e:
+                print("An exception occurred: ", e)
+                # The following lines appear to be commented out and may be removed or uncommented as needed.
+                #pd.to_datetime(self.df[col], errors='coerce').notna())
+                #df['date_column'] = pd.to_datetime(df['date_column']
 
-        if is_datetime:
-          self.df[col] = pd.to_datetime(self.df[col]).astype(int) / 10**9
-          if col not in self.continuous_cols:
-            self.continuous_cols.append(col)
-          self.categorical_cols.remove(col)
+            if is_datetime:
+                self.df[col] = pd.to_datetime(self.df[col]).astype(int) / 10**9
+                if col not in self.continuous_cols:
+                    self.continuous_cols.append(col)
+                self.categorical_cols.remove(col)
 
-      logging.info("Columns identified and converted where necessary.")
+        logging.info("Columns identified and converted where necessary.")
 
-      # Assertions to verify column names
-      assert all(col in self.df.columns for col in self.categorical_cols
-                 ), "Some categorical columns are not in DataFrame"
-      assert all(
-          col in self.df.columns for col in
-          self.continuous_cols), "Some continuous columns are not in DataFrame"
+        # Assertions to verify column names
+        assert all(col in self.df.columns for col in self.categorical_cols), "Some categorical columns are not in DataFrame"
+        assert all(col in self.df.columns for col in self.continuous_cols), "Some continuous columns are not in DataFrame"
     except Exception as e:
-      logging.error(f"Error in identifying and converting columns: {str(e)}")
-      raise
+        logging.error(f"Error in identifying and converting columns: {str(e)}")
+        raise
 
   def handle_missing_values(self):
     try:
@@ -202,90 +242,83 @@ class DataProcessor:
       logging.error(f"Error in preprocessing data: {str(e)}")
       raise
 
-  def perform_eda(self):
+  def perform_eda(self, target_variable_name=None):
     """
-        Perform exploratory data analysis on the dataset.
-        """
+    Perform exploratory data analysis on the dataset.
+    """
     try:
-      # Correlation Matrix Heatmap
-      #correlation_matrix = self.df.corr()
-      correlation_matrix = self.df.corr(numeric_only=True)
-
-      sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
-      plt.title('Correlation Matrix of All Features')
-      plt.show()
-
-      # Assuming you have a target variable to pass as hue
-      sns.pairplot(self.df, hue='target_variable_name'
-                   )  # replace with the actual target variable name
-
-      # Scatter Plot
-      plt.scatter(self.df['feature_1'], self.df['feature_2'])
-      plt.xlabel('Feature 1')
-      plt.ylabel('Feature 2')
-      plt.title('Scatter plot between Feature 1 and Feature 2')
-      plt.show()
-
-      # Boxplot
-      sns.boxplot(data=self.df[['feature_1', 'feature_2']])
-      plt.show()
-
-      # Identifying outliers using Z-Score
-      for column_name in self.df.select_dtypes(include=np.number).columns:
-        z_scores = np.abs(zscore(self.df[column_name]))
-        outliers_z = (z_scores > 3)
-        # Log or print some info about the outliers
-        num_outliers = np.sum(outliers_z)
-        print(
-            f"Feature: {column_name}, Outliers using Z-Score: {num_outliers}")
-
-      # Identifying outliers using IQR
-      # Loop through all numeric columns
-      for column_name in self.df.select_dtypes(include=np.number).columns:
-        # Identifying outliers using IQR
-        Q1 = self.df[column_name].quantile(0.25)
-        Q3 = self.df[column_name].quantile(0.75)
-        IQR = Q3 - Q1
-        outliers_iqr = ((self.df[column_name] <
-                         (Q1 - 1.5 * IQR)) | (self.df[column_name] >
-                                              (Q3 + 1.5 * IQR)))
-
-        # Log or print some info about the outliers
-        num_outliers = np.sum(outliers_iqr)
-        print(f"Feature: {column_name}, Outliers using IQR: {num_outliers}")
-
-        # Visualization: Boxplot with outliers marked
-        plt.figure(figsize=(10, 6))
-        sns.boxplot(x=self.df[column_name])
-        plt.scatter(np.where(outliers_iqr),
-                    self.df[column_name][outliers_iqr],
-                    marker='o',
-                    color='r',
-                    s=100)
-        plt.title(
-            f"Boxplot of {column_name} with Outliers Identified using IQR")
+        # Correlation Matrix Heatmap
+        correlation_matrix = self.df.corr(numeric_only=True)
+        sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+        plt.title('Correlation Matrix of All Features')
         plt.show()
 
-      # Maybe visualize or log the outliers here.
+        # Pairplot (only if target variable is provided)
+        if target_variable_name and target_variable_name in self.df.columns:
+            sns.pairplot(self.df, hue=target_variable_name)
+            plt.show()
+
+        # Scatter Plot for first two numeric features (just an example)
+        numeric_columns = self.df.select_dtypes(include=np.number).columns
+        if len(numeric_columns) > 1:
+            plt.scatter(self.df[numeric_columns[0]], self.df[numeric_columns[1]])
+            plt.xlabel(numeric_columns[0])
+            plt.ylabel(numeric_columns[1])
+            plt.title(f'Scatter plot between {numeric_columns[0]} and {numeric_columns[1]}')
+            plt.show()
+
+        # Boxplot for all numeric features
+        sns.boxplot(data=self.df[numeric_columns])
+        plt.show()
+
+        # Identifying outliers using Z-Score and IQR
+        for column_name in numeric_columns:
+            # Using Z-Score
+            z_scores = np.abs(zscore(self.df[column_name]))
+            outliers_z = (z_scores > 3)
+            num_outliers = np.sum(outliers_z)
+            print(f"Feature: {column_name}, Outliers using Z-Score: {num_outliers}")
+
+            # Using IQR
+            Q1 = self.df[column_name].quantile(0.25)
+            Q3 = self.df[column_name].quantile(0.75)
+            IQR = Q3 - Q1
+            outliers_iqr = ((self.df[column_name] < (Q1 - 1.5 * IQR)) | (self.df[column_name] > (Q3 + 1.5 * IQR)))
+            num_outliers = np.sum(outliers_iqr)
+            print(f"Feature: {column_name}, Outliers using IQR: {num_outliers}")
+
+            # Visualization: Boxplot with outliers marked using IQR
+            plt.figure(figsize=(10, 6))
+            sns.boxplot(x=self.df[column_name])
+            plt.scatter(np.where(outliers_iqr), self.df[column_name][outliers_iqr], marker='o', color='r', s=100)
+            plt.title(f"Boxplot of {column_name} with Outliers Identified using IQR")
+            plt.show()
 
     except Exception as e:
-      logging.error(f"Error performing exploratory data analysis: {str(e)}")
+        logging.error(f"Error performing exploratory data analysis: {str(e)}")
+
       
   def false_positive_rate(y_true, y_pred):
-        """
-        Compute the False Positive Rate.
-        
-        Parameters:
-        - y_true: True labels
-        - y_pred: Predicted labels
-        
-        Returns:
-        - False Positive Rate
-        """
-        FP = sum((y_true == 1) & (y_pred == -1))
-        TN = sum((y_true == 1) & (y_pred == 1))
-        return FP / (FP + TN)
+    """Compute False Positive Rate."""
+    # Determine unique labels
+    unique_labels = set(y_true) | set(y_pred)
+    if len(unique_labels) != 2:
+        raise ValueError("Expected exactly two unique labels")
 
+    # Determine which label is "positive" and which is "negative"
+    sorted_labels = sorted(list(unique_labels))
+    negative_label, positive_label = sorted_labels
+
+    # Define true negatives (TN), false positives (FP), true positives (TP), and false negatives (FN)
+    TN = sum((y_true == negative_label) & (y_pred == negative_label))
+    FP = sum((y_true == negative_label) & (y_pred == positive_label))
+    #TP = sum((y_true == positive_label) & (y_pred == positive_label))
+    #FN = sum((y_true == positive_label) & (y_pred == negative_label))
+
+    # Calculate FPR
+    FPR = FP / (FP + TN)
+
+    return FPR
 
 def main():
   sep_pattern = r'[;,\|\s]'
@@ -352,6 +385,9 @@ def main():
   if 'Class' in df.columns:
     outlier_fraction = len(df[df['Class'] == 'anomaly']) / len(df)
     pipeline.named_steps['model'].set_params(contamination=outlier_fraction)
+    
+    # Convert anomaly and normal labels to 1 and -1 respectively for Isolation Forest
+    df['Class'] = df['Class'].map({'anomaly': 1, 'normal': -1})
 
   X, y = (df.drop('Class', axis=1),
           df['Class']) if 'Class' in df.columns else (df, None)
@@ -362,26 +398,38 @@ def main():
     print('Average F1 Score from Cross Validation:', np.mean(cv_score))
 
   if y is not None:
-    X_train, X_val, _, y_val = train_test_split(X,
+    X_train, X_val, y_train, y_val = train_test_split(X,
                                                 y,
                                                 test_size=0.2,
                                                 random_state=42,
                                                 stratify=y)
   else:
     X_train, X_val = train_test_split(X, test_size=0.2, random_state=42)
-    y_val = None, None
+    y_train, y_val = None, None
 
-  pipeline.fit(X_train)
+  # Training the pipeline
+  pipeline.fit(X_train, y_train)
+  
+  # Making predictions on the test set
+  y_pred = pipeline.predict(X_val)
+  
+  # Evaluating the model
+  f1_scorer = make_scorer(f1_score, pos_label=1)
+  f1 = f1_scorer(pipeline, X_val, y_val)
+  print(f"F1 Score: {f1:.4f}")
+
+  # Computing the False Positive Rate (FPR)
+  fpr = DataProcessor.false_positive_rate(y_val, y_pred)
+  print(f"False Positive Rate (FPR): {fpr:.4f}")
+  
+  # Additionally, printing a classification report for a detailed overview
+  print("\nClassification Report:\n")
+  print(classification_report(y_val, y_pred))
 
   if y_val is not None:
     scores = pipeline.decision_function(X_val)
     threshold = 0.1  # You may have to adjust the threshold value based on your specific use case
     predictions = [-1 if score < threshold else 1 for score in scores]
-
-    # Compute and display the False Positive Rate
-    fp_rate = false_positive_rate(y_val, predictions)
-    print(f"False Positive Rate: {fp_rate:.3f}")
-    
     print(classification_report(y_val, predictions))
 
   scores = pipeline.decision_function(X)
